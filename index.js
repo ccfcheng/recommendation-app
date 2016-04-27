12b0d469dcd68c2f8bd21dfb4ecfb3f896c6a0d1
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-04-26 18:14:55 -0700

    feat(initial setup): Basic skeleton of React files
    - Static files served by Express server
    - Initial GET route to / opens the index.html of app
    - Webpack bundles files in ./app/src using Babel for ES2015 transpiling
    
    TODO:
    - Use webpack-dev-server for faster incremental builds

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname,'/app/dist')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/app/dist/index.html'));
+});
+
+app.listen(8080, function(){
+  console.log("Started listening on port", 8080);
+});
