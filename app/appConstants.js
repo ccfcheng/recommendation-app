1b622afd14445ff5ad41d80c429e16685e97cae7
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-05-11 08:46:49 -0700

    Closes #72 - Uses correct endpoint regardless of hosting environment (#74)

--- app/appConstants.js
+++ app/appConstants.js
@@ -1,17 +1,15 @@
 export const FIREBASE_URL = 'https://glowing-torch-3956.firebaseio.com';
 
-const DEV_URL = 'http://localhost:8080';
+const SEARCH_ENDPOINT = 'api/search?';
 
-const BASE_URL = '//flavr.herokuapp.com';
+export const YELP_SEARCH_ENDPOINT = window.location.href + SEARCH_ENDPOINT;
 
-export const DEV_YELP_SEARCH_ENDPOINT = DEV_URL + '/api/search?';
+console.log('appConstants using search endpoint:', YELP_SEARCH_ENDPOINT);
 
-export const YELP_SEARCH_ENDPOINT = BASE_URL + '/api/search?';
+// export const USERS_URL = FIREBASE_URL + '/users';
 
-export const USERS_URL = FIREBASE_URL + '/users';
+// export const SPLASH_URL = 'http://cdn.allwallpaper.in/wallpapers/414x736/3168/restaurant-streets-414x736-wallpaper.jpg';
 
-export const SPLASH_URL = 'http://cdn.allwallpaper.in/wallpapers/414x736/3168/restaurant-streets-414x736-wallpaper.jpg';
+// export const SPLASH_URL2 = 'http://www.ilikewallpaper.net/iphone-5-wallpapers/download/12861/Blurred-lights-iphone-5-wallpaper-ilikewallpaper_com.jpg';
 
-export const SPLASH_URL2 = 'http://www.ilikewallpaper.net/iphone-5-wallpapers/download/12861/Blurred-lights-iphone-5-wallpaper-ilikewallpaper_com.jpg';
-
-export const SPLASH_URL3 = 'http://www.freeapplewallpapers.com/wp-content/uploads/2014/01/Blurred-Halos.jpg';
+// export const SPLASH_URL3 = 'http://www.freeapplewallpapers.com/wp-content/uploads/2014/01/Blurred-Halos.jpg';
