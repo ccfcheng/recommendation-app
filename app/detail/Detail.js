2b328998f05bfb572ce872da33151b63debe6717
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-05-16 16:04:57 -0700

    Closes #79
    - Add clickable telephone and driving directions links
    - Remove text decoration from links
    - Add currentLatitude and currentLongitude for redux state

--- app/detail/Detail.js
+++ app/detail/Detail.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {Card, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
-export default class DetailContainer extends Component {
+class DetailContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,6 +20,8 @@ export default class DetailContainer extends Component {
       <Detail
         business={this.props.business}
         handleExpandChange={this.handleExpandChange}
+        slat={this.props.currentLatitude}
+        slng={this.props.currentLongitude}
       />
     );
   }
@@ -26,9 +29,19 @@ export default class DetailContainer extends Component {
 
 class Detail extends Component {
   render() {
+
     const {
       business,
+      slat,
+      slng,
     } = this.props;
+
+    const phoneURL = 'tel://' + business.display_phone;
+    const phoneLink = <a href={phoneURL}>{business.display_phone}</a>;
+    const dlat = business.location.coordinate.latitude;
+    const dlng = business.location.coordinate.longitude;
+    const mapURL = 'comgooglemaps://?saddr=' + slat + ',' + slng + '&daddr=' + dlat + ',' + dlng + '&directionsmode=driving';
+    const mapLink = <a href={mapURL}>{business.location.display_address.join(', ')}</a>;
 
     return (
       <Card
@@ -48,14 +61,14 @@ class Detail extends Component {
           expandable={true}
           overlay={<CardTitle
                     title={business.categories[0][0]}
-                    subtitle={business.location.display_address.join(', ')}
+                    subtitle={mapLink}
                   />}
         >
           <img src={business.image_url.replace('ms.jpg', 'ls.jpg')} />
         </CardMedia>
 
         <CardTitle
-          subtitle={business.display_phone}
+          subtitle={phoneLink}
           expandable={true}
         />
 
@@ -67,3 +80,12 @@ class Detail extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentLatitude: state.search.currentLatitude,
+    currentLongitude: state.search.currentLongitude,
+  };
+}
+
+export default connect(mapStateToProps)(DetailContainer);
